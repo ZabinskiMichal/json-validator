7aab280a694d1665998b637d21e2133a3c1ee2e0
Michal Zabinski <zabinski@student.agh.edu.pl>
2024-03-26 14:49:59 +0100

    documentation for validation functions added

--- src/main/java/com/zabinski/FieldsValidation/ResourceFieldVerifier.java
+++ src/main/java/com/zabinski/FieldsValidation/ResourceFieldVerifier.java
@@ -13,12 +13,25 @@ import java.io.IOException;
 public class ResourceFieldVerifier implements JsonVerifier {
 
 
+
     /**
+     * Verifies the JSON file located at the specified filePath against the validation criteria.
      *
-     * @param filePaths
-     * @return
-     * should return false only when "Resource": "*"
-     * @throws IOException
+     * This method performs several validations:
+     * 1. Validates the file existence and extension using FileValidator.
+     * 2. Ensures the JSON structure contains a "PolicyDocument" with a "Statement" array.
+     * 3. Checks each "Statement" for a "Resource" field. If the "Resource" field is missing,
+     *    it throws a {@link ResourceFieldNotFoundException}.
+     * 4. If any "Resource" field contains a single asterisk "*", the method returns false,
+     *    indicating the JSON file does not meet the validation criteria.
+     *
+     * If all "Resource" fields are valid and no "*" is found, the method returns true.
+     *
+     * @param filePath The path to the JSON file being verified. Must not be null.
+     * @return true if the JSON file meets the validation criteria, otherwise false.
+     * @throws IOException If an I/O error occurs reading from the file.
+     * @throws IllegalArgumentException If the JSON structure is invalid or a required node is missing.
+     * @throws ResourceFieldNotFoundException If the "Resource" field is missing in any "Statement".
      */
     @Override
     public boolean verify(String filePath) throws IOException {
--- src/main/java/com/zabinski/fileValidation/FileValidator.java
+++ src/main/java/com/zabinski/fileValidation/FileValidator.java
@@ -8,6 +8,20 @@ import java.nio.file.Paths;
 
 public class FileValidator {
 
+
+    /**
+     * Validates if the specified file exists and is a JSON file.
+     *
+     * This method performs two main validations:
+     * 1. Checks if the file at the provided path exists in the file system.
+     *    If the file does not exist, it throws a {@link FileNotFoundException}.
+     * 2. Checks if the file has a .json extension.
+     *    If the file does not have a .json extension, it throws an {@link InvalidFileExtensionException}.
+     *
+     * @param filePath The path to the file being validated. Must not be null.
+     * @throws FileNotFoundException If the file at the specified path does not exist.
+     * @throws InvalidFileExtensionException If the file does not have a .json extension.
+     */
     public static void validateFile(String filePath) throws FileNotFoundException, InvalidFileExtensionException {
 
         if(!Files.exists(Paths.get(filePath))){
